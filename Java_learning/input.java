53a654f1cb1d5854373a71e94cf0b4bcbf9c8078
Pratik Patil <pratik0898patil@gmail.com>
2024-08-11 12:56:40 +0530

    Git commit -Day1

---
+++ Java_learning/input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class input {
+     public static void main(String[]args) {
+    	 Scanner sc=new Scanner(System.in);
+    	 System.out.println("Enter your roll no:");
+    	 int rollno= sc.nextInt();
+    	 System.out.println("My roll no is "+ rollno);
+     }
+}
